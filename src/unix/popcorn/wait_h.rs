c7db77e650fc2cf1d956ffada4a7f1af605121cd
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-02-12 19:19:31 +0000

    fixup

--- src/unix/popcorn/wait_h.rs
+++ src/unix/popcorn/wait_h.rs
@@ -10,34 +10,34 @@ pub const __WCLONE: ::c_int = 0x80000000;
 
 pub const WCOREFLAG: ::c_int = 0x80;
 
-pub {const} fn WIFSTOPPED(status: ::c_int) -> bool {
+pub const fn WIFSTOPPED(status: ::c_int) -> bool {
 	(status & 0xff) == 0x7f
 }
 
-pub {const} fn WSTOPSIG(status: ::c_int) -> ::c_int {
+pub const fn WSTOPSIG(status: ::c_int) -> ::c_int {
 	WEXITSTATUS(status)
 }
 
-pub {const} fn WIFCONTINUED(status: ::c_int) -> bool {
+pub const fn WIFCONTINUED(status: ::c_int) -> bool {
 	status == 0xffff
 }
 
-pub {const} fn WIFSIGNALED(status: ::c_int) -> bool {
+pub const fn WIFSIGNALED(status: ::c_int) -> bool {
 	((((status & 0x7f) + 1) as i8) >> 1) > 0
 }
 
-pub {const} fn WTERMSIG(status: ::c_int) -> ::c_int {
+pub const fn WTERMSIG(status: ::c_int) -> ::c_int {
 	status & 0x7f
 }
 
-pub {const} fn WIFEXITED(status: ::c_int) -> bool {
+pub const fn WIFEXITED(status: ::c_int) -> bool {
 	WTERMSIG(status) == 0
 }
 
-pub {const} fn WEXITSTATUS(status: ::c_int) -> ::c_int {
+pub const fn WEXITSTATUS(status: ::c_int) -> ::c_int {
 	(status & 0xff) >> 8
 }
 
-pub {const} fn WCOREDUMP(status: ::c_int) -> bool {
+pub const fn WCOREDUMP(status: ::c_int) -> bool {
 	(status & ::WCOREFLAG) != 0
 }
